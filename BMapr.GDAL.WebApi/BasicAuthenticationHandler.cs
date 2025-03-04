b74b650ff33e1e16b1ba9d25cf350bfb8ea39320
bad <bgd5074@gmail.com>
2025-03-04 22:01:22 +0100

    Suppress some warnings, move value to appsettings.json

--- BMapr.GDAL.WebApi/BasicAuthenticationHandler.cs
+++ BMapr.GDAL.WebApi/BasicAuthenticationHandler.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.RegularExpressions;
+using BMapr.GDAL.WebApi.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 
@@ -12,8 +13,11 @@ namespace BMapr.GDAL.WebApi
     /// </summary>
     public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
     {
-        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        private readonly IConfiguration _config;
+
+        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, IConfiguration iConfig) : base(options, logger, encoder)
         {
+            _config = iConfig;
         }
 
         protected override Task<AuthenticateResult> HandleAuthenticateAsync()
@@ -34,12 +38,14 @@ namespace BMapr.GDAL.WebApi
                 return Task.FromResult(AuthenticateResult.Fail("Authorization code not formatted properly."));
             }
 
+            var systemPw = _config.GetSection("Settings").GetSection("PwWebAdmin").Value;
+
             var authBase64 = Encoding.UTF8.GetString(Convert.FromBase64String(authHeaderRegex.Replace(authorizationHeader, "$1")));
             var authSplit = authBase64.Split(Convert.ToChar(":"), 2);
             var authUsername = authSplit[0];
             var authPassword = authSplit.Length > 1 ? authSplit[1] : throw new Exception("Unable to get password");
 
-            if (authUsername != "webadmin" || authPassword != "djkswdhj56345hfhHH?2342jrehd")
+            if (authUsername != "webadmin" || authPassword != systemPw)
             {
                 return Task.FromResult(AuthenticateResult.Fail("The username or password is not correct."));
             }
