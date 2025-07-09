30dba6334fc7a61cb29190ae5b33590237a1d0b4
bad <bgd5074@gmail.com>
2025-07-09 23:25:23 +0200

    Prepare an abstract class for "OGC API Feature" service

---
+++ BMapr.GDAL.WebApi/Models/OgcApi/Features/CollectionItem.cs
@@ -0,0 +1,29 @@
+﻿namespace BMapr.GDAL.WebApi.Models.OgcApi.Features
+{
+    public class CollectionItem
+    {
+        public static readonly string DEFAULT_CRS = "http://www.opengis.net/def/crs/OGC/1.3/CRS84";
+
+        public string Name { get; set; } // collection id
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string SpatialCrs { get; set; } = DEFAULT_CRS;
+
+        public string StorageCrs { get; set; } = DEFAULT_CRS;
+
+        public string DefaultCrs { get; set; } = DEFAULT_CRS;
+
+        public List<string> AdditionalCrs { get; set; } = new ();
+
+        public double MinX { get; set; }
+
+        public double MinY { get; set; }
+
+        public double MaxX { get; set; }
+
+        public double MaxY { get; set; }
+    }
+}
---
+++ BMapr.GDAL.WebApi/Services/OgcApi/OgcApiFeatureBase.cs
@@ -0,0 +1,93 @@
+﻿using BMapr.GDAL.WebApi.Models;
+using BMapr.GDAL.WebApi.Models.OgcApi.Features;
+using BMapr.GDAL.WebApi.Models.Spatial;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BMapr.GDAL.WebApi.Services.OgcApi
+{
+    public abstract class OgcApiFeatureBase
+    {
+        public static readonly string JsonMimeType = "application/json";
+        public static readonly string GeoJsonMimeType = "application/geo+json";
+        public static readonly string SchemaJsonMimeType = "application/schema+json";
+        public static readonly string EpsgCrsPrefix = "http://www.opengis.net/def/crs/EPSG/0/";
+
+        private string UrlCollections { get; set; }
+        private List<CrsDefinition> CrsDefinitions { get; set; }
+
+        protected OgcApiFeatureBase(string urlCollections, List<CrsDefinition> crsDefinitions)
+        {
+            UrlCollections = urlCollections;
+            CrsDefinitions = crsDefinitions;
+        }
+
+        public Result<Collections> GetCollections(List<CollectionItem> items)
+        {
+            var collections = new Collections();
+
+            items.ForEach(item =>
+            {
+                collections.CollectionList.Add(GetCollectionItem(item).Value);
+            });
+
+            return new Result<Collections> { Value = collections, Succesfully = true };
+        }
+
+
+        public Result<Collection> GetCollection(CollectionItem item)
+        {
+            var collection = GetCollectionItem(item);
+            return new Result<Collection> { Value = collection.Value, Succesfully = true };
+        }
+
+
+        private Result<Collection> GetCollectionItem(CollectionItem item)
+        {
+            var collection = new Collection
+            {
+                Id = item.Name,
+                Title = item.Title,
+                Description = item.Description,
+                Extent = new BMapr.GDAL.WebApi.Models.OgcApi.Features.Extent
+                {
+                    Temporal = new Temporal(),
+                    Spatial = new Spatial
+                    {
+                        Crs = item.SpatialCrs,
+                    },
+                },
+            };
+
+            collection.StorageCrs = item.StorageCrs;
+            collection.Crs.Add(item.DefaultCrs);
+
+            var listLayer = GetCrsList(item.AdditionalCrs);
+
+            if (listLayer.Count > 0)
+            {
+                collection.Crs.AddRange(listLayer);
+            }
+
+            collection.Crs = collection.Crs.Distinct().ToList();
+
+            collection.Extent.Spatial.Bbox.Add(new List<double> { item.MinX, item.MinY, item.MaxX, item.MaxY });
+
+            collection.Links.Add(new Link { Rel = "self", Title = "This collection", Type = JsonMimeType, Href = $"{UrlCollections}/{item.Name}" });
+            collection.Links.Add(new Link { Rel = "items", Title = $"{item.Name} as GeoJSON", Type = GeoJsonMimeType, Href = $"{UrlCollections}/{item.Name}/items?f=geojson" });
+            collection.Links.Add(new Link { Rel = "queryables", Title = $"Get available attributes", Type = SchemaJsonMimeType, Href = $"{UrlCollections}/{item.Name}/queryables?f=json" });
+            // collection.Links.Add(new Link() { Rel = "describedby", Title = $"Schema for {item.Name}", Type = JsonMimeType, Href = $"{UrlCollections}/{item.Name}/schema?f=application/json" });
+
+            return new Result<Collection> { Value = collection, Succesfully = true };
+        }
+
+        private List<string> GetCrsList(List<string> crsList)
+        {
+            var crsListFormated = crsList.Select(x => x.Replace("epsg:", "")).Select(x => $"{EpsgCrsPrefix}{x}").ToList();
+            return crsListFormated.Distinct().ToList();
+        }
+
+        public abstract Result<FileContentResult> GetItems(GetItemRequest request);
+
+        public abstract Result<Schema> GetQueryables(GetQueryablesRequest request);
+    }
+}
