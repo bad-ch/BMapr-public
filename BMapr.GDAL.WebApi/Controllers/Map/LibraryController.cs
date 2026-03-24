20bf3ce1c11b9caafa8148ca2d0396c051876450
bgd5074 <bgd5074@gmail.com>
2026-03-24 23:47:38 +0100

    Add library endpoint for symbols

---
+++ BMapr.GDAL.WebApi/Controllers/Map/LibraryController.cs
@@ -0,0 +1,25 @@
+﻿using BMapr.GDAL.WebApi.Models;
+using BMapr.GDAL.WebApi.Models.Map;
+using BMapr.GDAL.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BMapr.GDAL.WebApi.Controllers.Map;
+
+[ApiController]
+[Route("api/map/library")]
+public class LibraryController(
+    ILogger<LegendController> logger,
+    IConfiguration config,
+    IWebHostEnvironment environment)
+    : DefaultController(config, environment)
+{
+    private readonly ILogger<LegendController> _logger = logger;
+
+    [HttpGet("symbol")]
+    public IActionResult GetLibrary()
+    {
+        var filePath = Path.Combine(Config.DataShare.FullName, "library", "symbols.json");
+        var fileContent = System.IO.File.ReadAllBytes(filePath);
+        return new FileContentResult(fileContent, "application/json");
+    }
+}
---
+++ BMapr.GDAL.WebApi/Models/Map/LibraryItemSymbol.cs
@@ -0,0 +1,11 @@
+﻿using BMapr.GDAL.WebApi.Services;
+
+namespace BMapr.GDAL.WebApi.Models.Map
+{
+    public class LibraryItemSymbol
+    {
+        public string Name { get; set; }
+
+        public SymbolObj Symbol { get; set; }
+    }
+}
